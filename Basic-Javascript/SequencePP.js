c297a66a9a960420faaae7167cf535dbeb4f8eea
sohail <sohailqureshi82@gmail.com>
2021-07-14 18:15:42 +0530

    [sohail] Add: ability to perform sequence programming in javascript

---
+++ Basic-Javascript/SequencePP.js
@@ -0,0 +1,7 @@
+// 1 - Sequence Practice Problem
+console.log("Single digit number "+ Math.floor(Math.random() * 10));
+//2
+console.log("Dice Number "+ Math.floor(1 + Math.random()*5));
+//3
+let num =(Math.floor(Math.random() * 10)) + (Math.floor(Math.random() * 10));
+console.log(num);
