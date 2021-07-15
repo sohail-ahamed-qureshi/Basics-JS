6784bd529e8adefde2e84dada8a3897723d9cd64
sohail <sohailqureshi82@gmail.com>
2021-07-15 11:39:35 +0530

    [sohail] ADD: abilty to perform coin simulation

--- Basic-Javascript/SelectionPP.js
+++ Basic-Javascript/SelectionPP.js
@@ -1,9 +1,11 @@
 console.log("press 1 to Generate random number and check min and max");
 console.log("press 2 check if day and month is between march 20 and july 20");
 console.log("press 3 check the year is leap year or not");
+console.log("press 4 simulate Coin flip");
+
 var readlineSync = require("readline-sync");
 let input = readlineSync.questionInt();
-while (input > 3 || input <= 0) {
+while (input > 4 || input <= 0) {
   console.log("Invalid input");
   input = readlineSync.questionInt();
 }
@@ -14,27 +16,39 @@ switch (input) {
   case 2:
     CheckDayandMonth();
     break;
-    case 3:
+  case 3:
     CheckLeapYear();
+    break;
+  case 4:
+    CoinFlip();
     break;
   default:
     console.log("Invalid Inupt");
     break;
 }
 
+//ability to simulate coin flip
+function CoinFlip(){
+  const HEAD = 1;
+    let coin = Math.floor(Math.random() * 2)
+    if(coin == HEAD)
+    console.log("HEAD");
+    else
+    console.log("TAILS");
+}
+
 //ability to check whether a year is leap year or not
-function CheckLeapYear(){
-    let year = readlineSync.question("Enter a year: ");
-    while (year > 9999 || year <= 999){
-        console.log("Invalid year, Try again. \n year must be a 3 digit number");
-        year = readlineSync.question("Enter a year: ");
-    }
-    if(year % 4 == 0 || year % 100 != 0 && year % 4 ==0 ){
-        console.log(year," is leap year ");
-    }
-    else{
-        console.log(year," is not a leap year");
-    }
+function CheckLeapYear() {
+  let year = readlineSync.question("Enter a year: ");
+  while (year > 9999 || year <= 999) {
+    console.log("Invalid year, Try again. \n year must be a 3 digit number");
+    year = readlineSync.question("Enter a year: ");
+  }
+  if (year % 4 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+    console.log(year, " is leap year ");
+  } else {
+    console.log(year, " is not a leap year");
+  }
 }
 
 //2 check if day and month is between march 20 and july 20
@@ -50,10 +64,13 @@ function CheckDayandMonth() {
     console.log("invalid month, Enter again between 1-12");
     month = readlineSync.questionInt("Enter the month; ");
   }
-  if(((month == 3 && day >= 20) || (month == 6 && day <= 20)) || (month > 3 && month < 6 ) ){
-      console.log("true");
-  }
-  else{
+  if (
+    (month == 3 && day >= 20) ||
+    (month == 6 && day <= 20) ||
+    (month > 3 && month < 6)
+  ) {
+    console.log("true");
+  } else {
     console.log("false");
   }
 }
